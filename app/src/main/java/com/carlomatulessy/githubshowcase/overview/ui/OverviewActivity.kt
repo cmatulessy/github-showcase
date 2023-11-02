f2c4c3f8e5773f14f2ab4823cfab92808d83a36c
The Carlo <carlomatulessy@gmail.com>
2023-11-02 08:34:32 +0100

    add compose wip

---
+++ app/src/main/java/com/carlomatulessy/githubshowcase/overview/ui/OverviewActivity.kt
@@ -0,0 +1,46 @@
+package com.carlomatulessy.githubshowcase.overview.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.carlomatulessy.githubshowcase.overview.ui.ui.theme.GitHubShowCaseTheme
+
+class OverviewActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GitHubShowCaseTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GitHubShowCaseTheme {
+        Greeting("Android")
+    }
+}
