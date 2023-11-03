1fce97de1314074dc70df6c3213f1bfc41aeea4f
The Carlo <carlomatulessy@gmail.com>
2023-11-03 09:24:42 +0100

    wip with fixed gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
+        kotlinCompilerExtensionVersion = "1.5.2"
     }
     packaging {
         resources {
